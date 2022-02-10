f1cb0b44e406dbe47d6f864d04f838fddb9147fb
kickitlb <parandrez@gmail.com>
2022-02-10 12:37:00 -0700

    work in progress

--- index.js
+++ index.js
@@ -1,54 +1,95 @@
-const computerInput = ['rock', 'paper', 'scissors'];
-const computerSelection = computerInput[Math.floor(Math.random() * computerInput.length)]; 
 
 // function that returns a random value of rock paper or scissors
 function computerPlays() {
+    const computerInput = ['rock', 'paper', 'scissors'];
+    const computerSelection = computerInput[Math.floor(Math.random() * computerInput.length)]; 
     return computerSelection;
 }
-computerPlays();
-
-console.log(computerPlays());
+// computerPlays();
+// console.log(computerPlays());
 
 
 // stores the value of rock paper or scissors inside playerSelection
 const playerSelection = prompt('Rock, Paper, or Scissors?');
 
 
-// rules defining rock priority
-function rock() {
-    if (computerSelection == 'rock' && playerSelection == 'paper') {
-        alert('You win! Paper beats rock!');
-    } else if ((computerSelection == 'rock') && (playerSelection == 'scissors')) {
-        alert('You lose! Rock beats paper!');
+function rules() {
+    computerPlays();
+    console.log(computerPlays());
+    if (computerPlays() === 'rock') {
+        if (computerPlays() === 'rock' && playerSelection == 'paper') {
+            alert('You win! Paper beats rock!');
+        } else if (computerPlays() ==='rock' && playerSelection == 'scissors') {
+            alert('You lose! Rock beats scissors');
+        } else if (computerPlays() === 'rock' && playerSelection == 'rock') {
+            alert('draw');
+        }
+    } else if (computerPlays() === 'paper') {
+        if (computerPlays() === 'paper' && playerSelection == 'scissors') {
+            alert('You win! Scissors beats paper!');
+        } else if (computerPlays() === 'paper' && playerSelection == 'rock') {
+            alert('You lose! Paper beats scissors!');
+        } else if (computerPlays() === 'paper' && playerSelection == 'paper') {
+            alert('draw');
+        }
+    }else if (computerPlays() === 'scissors') {
+        if (computerPlays() === 'scissors' && playerSelection == 'rock') {
+            alert('You win! Rock beats scissors!'); 
+        } else if (computerPlays() === 'scissors' && playerSelection == 'paper') {
+            alert('You lose! Scissors beats paper!');
+        } else if (computerPlays() === 'scissors' && playerSelection == 'scissors') {
+            alert('draw');
+        }
+    } else {
+        alert('error');
     }
+    console.log(playerSelection);
 }
+rules();
 
-// rules defining paper priority
-function paper() {
-    if (computerSelection == 'paper' && playerSelection == 'scissors') {
-        alert('You win! Scissors beats paper!')
-    } else if ((computerSelection == 'paper') && (playerSelection == 'rock')) {
-        alert('You lose! Paper beats rock!');
-    }
-}
 
-// rules defining scissors priority 
-function scissors() {
-    if (computerSelection == 'scissors' && playerSelection == 'rock') {
-        alert('You win! Rock beats scissors!');
-    } else if ((computerSelection == 'scissors') && (playerSelection == 'paper')) {
-        alert('You lose! Scissors beats paper!');
-    }
-}
+// // rules defining rock priority
+// function rock() {
+//     if (computerPlays() == 'rock' && playerSelection == 'paper') {
+//         alert('You win! Paper beats rock!');
+//     } else if (computerPlays() == 'rock' && playerSelection == 'scissors') {
+//         alert('You lose! Rock beats scissors!');
+//     } else {
 
-function round() {
-    if (rock(), paper(), scissors() == true) {
+//     }
+// }
 
-    } else if ( computerSelection == playerSelection) {
-        alert('draw');
-    } else if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-        alert('invalid input');
-    }
-}
+// // rules defining paper priority
+// function paper() {
+//     if (computerPlays() == 'paper' && playerSelection == 'scissors') {
+//         alert('You win! Scissors beats paper!')
+//     } else if (computerPlays() == 'paper' && playerSelection == 'rock') {
+//         alert('You lose! Paper beats rock!');
+//     } else {
 
-round();
+//     }
+// }
+
+// // rules defining scissors priority 
+// function scissors() {
+//     if (computerPlays() == 'scissors' && playerSelection == 'rock') {
+//         alert('You win! Rock beats scissors!');
+//     } else if (computerPlays() == 'scissors' && playerSelection == 'paper') {
+//         alert('You lose! Scissors beats paper!');
+//     } else {
+
+//     }
+// }
+
+// function round() {
+//     if (rock(), paper(), scissors() == true) {
+
+//     } else if ( computerPlays() == playerSelection) {
+//         alert('draw');
+//     } else if (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+//         alert('invalid input');
+//     }
+// }
+
+
+// round();
